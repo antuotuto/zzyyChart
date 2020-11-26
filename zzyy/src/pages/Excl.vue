85490bc227239b7eb65879af11ebdbb069877d7e
Anan <675988829@qq.com>
2020-11-26 13:41:59 +0800

    asdha

--- zzyy/src/pages/Excl.vue
+++ zzyy/src/pages/Excl.vue
@@ -1,77 +1,58 @@
 <template>
-    <div>
-        <template>
-            <el-table
-            :data="tableData"
-            style="width: 100%">
-            <el-table-column
-                prop="date"
-                label="日期"
-                width="180">
-            </el-table-column>
-            <el-table-column
-                prop="name"
-                label="姓名"
-                width="180">
-            </el-table-column>
-            <el-table-column
-                prop="address"
-                label="地址">
-            </el-table-column>
-            </el-table>
-        </template>
-        <input type="button" value="加载" @click="excl">
-    </div>
+  <div>
+    <form>
+      <input
+        type="text"
+        value=""
+        v-model="warehouse.type"
+        placeholder="请输入上传类型"
+      />
+      <input type="file" @change="getFile($event)" />
+      <button @click="submitForm($event)">提交</button>
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-      data() {
-        return {
-          tableData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }],
-          list:{},
-          warehouse:{
-              startDate:"",
-              endDate:""
-          }
-        }
+export default {
+  data() {
+    return {
+      warehouse: {
+        type: "",
       },
-      methods:{
-          excl(){
-            
-            var te=this.warehouse
-            debugger
-              this.$axios({
-                  methods:'post',
-                    url:'http://192.168.10.7:8081/sales/getSalesWarehouseOutData',
-                    data:this.warehouse
-              }).then(response=>{
-                  this.list=response.date
-                  console.log(list)
-              })
-          }
+    };
+  },
+  methods: {
+    getFile(event) {
+      this.file = event.target.files[0];
+      console.log(this.file);
+    },
+    submitForm(event) {
+      event.preventDefault();
+      let formData = new FormData();
+      formData.append("type", this.warehouse.type);
+      formData.append("file", this.file);
+      let config = {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        },
+      };
 
-      }
-    }
-  </script>
+      this.$axios({
+        method: "post",
+        url: "http://192.168.10.7:8081/sales/getExcleWarehousing",
+        data: formData,
+        headers: config,
+      })
+        .then((response) => {
+          alert("上传完成");
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    },
 
-<style>
-
-
-</style>
+    
+  },
+};
+</script>
